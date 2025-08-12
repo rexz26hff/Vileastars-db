63ea82502d5c4a295489df17b50e0e6e5f944b68
rexz26hff <bg185967@gmail.com>
2025-08-12 17:33:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 {
 "tokens": [ 
   "8103529028:AAGWdf_P6So2z9OmBWd0Go9nmLBj6UxBYms",
-  "8045257375:AAFCc75-Fk5kmE13Qs57Jg2Getbw_0sHpHM"
+  "8157399522:AAG08iykujkh_3ef0GYpL2_8miLRniYw7oc"
           ] 
 }
